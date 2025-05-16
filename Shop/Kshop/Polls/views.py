063ca9a14c595b9fd8b68574c770683c0fb1a92f
qweriikk <dashakots13@gmail.com>
2025-05-17 01:27:41 +0300

    исправление поиска

--- Shop/Kshop/Polls/views.py
+++ Shop/Kshop/Polls/views.py
@@ -1,5 +1,6 @@
 from typing import Any
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login as auth_login
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
